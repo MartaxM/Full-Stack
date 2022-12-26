45fd0bbb60d2296745fdf9f3397c8c0f33106126
Marta X. Moraga <martax13mh@gmail.com>
2022-12-26 16:20:59 +0100

    NodeJSCrashCourse: Done with reference folder

--- Coursework/1NodeJSCrashCourse/index.js
+++ Coursework/1NodeJSCrashCourse/index.js
@@ -1,14 +1,24 @@
-console.log('Hello From Node.js');
+// console.log('Hello From Node.js');
 
-/*const person = require('./person');
-console.log(person.name);*/
-
-
-const Person = require('./person');
-//import Person from './person'; is not possible
-
-const person1 = new Person('John Doe', 30);
+// /*const person = require('./person');
+// console.log(person.name);*/
 
 
+// const Person = require('./person');
+// //import Person from './person'; is not possible
 
-person1.greeting();
+// const person1 = new Person('John Doe', 30);
+
+
+
+// person1.greeting();
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener:', data));
+
+logger.log('Hello World');
+logger.log('Hi');
+logger.log('Hello');
