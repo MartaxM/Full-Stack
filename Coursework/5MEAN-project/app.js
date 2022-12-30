e332ce7d1692f05ccbc23e097e8213c64a6c278e
Marta X. Moraga <martax13mh@gmail.com>
2022-12-30 14:16:29 +0100

    MEAN-project part 4

--- Coursework/5MEAN-project/app.js
+++ Coursework/5MEAN-project/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors'); 
+const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const session = require('express-session');
 
 // Connect To Database
 //mongoose.set('strictQuery', false);
@@ -12,12 +13,12 @@ mongoose.set('strictQuery', true);
 mongoose.connect(config.database);
 
 // On Connection
-mongoose.connection.on('connected', () =>{
+mongoose.connection.on('connected', () => {
     console.log('Connected to database' + config.database);
 });
 
 // On Error
-mongoose.connection.on('error', (err) =>{
+mongoose.connection.on('error', (err) => {
     console.log('Database error' + err);
 });
 
@@ -27,20 +28,32 @@ const users = require('./routes/users');
 
 const port = 3000;
 
-// Middleware
-    // CORS
+// CORS Middleware
 app.use(cors()); // allows request to API from different domian name
-    // Body Parser
+
+// Body Parser Middleware
 app.use(bodyParser.json())
 
+// Passport Middleware
+
+app.use(session({
+    secret: config.secret,
+    saveUninitialized: true,
+    resave: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
 // Set Static Folder
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/users', users);
 
 // Index Route
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Invalid Endpoint");
 });
 
--- Coursework/5MEAN-project/routes/users.js
+++ Coursework/5MEAN-project/routes/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
+const config = require('../config/database');
 const User = require('../models/users');
+const { session } = require('passport');
 
 // Register
 router.post('/register', (req, res, next) => {
@@ -26,12 +28,48 @@ router.post('/register', (req, res, next) => {
 
 // Authenticate
 router.post('/authenticate', (req, res, next) => {
-    res.send("AUTHENTICATE");
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.getUserByUsername(username, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            return res.json({ success: false, msg: 'User not found' });
+        }
+
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+
+                const token = jwt.sign(user.toJSON(), config.secret, {
+                    expiresIn: 604800
+                });
+
+                // const token = jwt.sign(user, config.secret, {
+                //     expiresIn: 604800, // 1 week
+                // });
+
+                res.json({
+                    success: true,
+                    token: 'JWT ' + token,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                    }
+                })
+            } else {
+                return res.json({ success: false, msg: 'Wrong password' });
+            }
+        });
+
+    })
 });
 
 // Profile
-router.get('/profile', (req, res, next) => {
-    res.send("PROFILE");
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+    res.json({user: req.user});
 });
 
 // Validate (Not necessary)
